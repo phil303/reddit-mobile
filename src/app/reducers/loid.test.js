3eed5b465a6a6403f2e4ef988964aa3ca730f161
Phil Aquilina <philaquilina@gmail.com>
2016-05-29 17:12:11 -0700

    add loid tests

---
+++ src/app/reducers/loid.test.js
@@ -0,0 +1,24 @@
+import createTest from '@r/platform/createTest';
+
+import * as loidAction from 'app/actions/loid';
+import loid from './loid';
+
+createTest({ reducers: { loid } }, ({ getStore, expect }) => {
+  describe('loid', () => {
+    describe('SET_LOID', () => {
+      it('should set loid', () => {
+        const LOID = 'EbxVm9pOhRDdk0Ck7S';
+        const CREATED = '2016-05-27T05:05:49.012Z';
+        const { store } = getStore();
+        store.dispatch({
+          type: loidAction.SET_LOID,
+          loid: LOID,
+          loidcreated: CREATED,
+        });
+        const { loid: { loid, loidcreated } } = store.getState();
+        expect(loid).to.equal(LOID)
+        expect(loidcreated).to.equal(CREATED)
+      });
+    });
+  });
+});
