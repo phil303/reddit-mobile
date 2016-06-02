d790c82be36d594524f3c321bc33be9719668994
Jack Lawson ajacksified@gmail.com <ajacksified@gmail.com>
2016-06-02 15:06:22 -0700

    [wip pageviews]

--- src/app/router/handlers/CommunityGotoAction.js
+++ src/app/router/handlers/CommunityGotoAction.js
@@ -1,7 +1,11 @@
 import { BaseHandler, METHODS } from '@r/platform/router';
 import * as communityGotoActions from 'app/actions/communityGoto';
 
+export const NAME = 'CommunityGotoAction';
+
 export default class CommunityGotoAction extends BaseHandler {
+  name = NAME;
+
   async [METHODS.POST](dispatch) {
     const location = (this.bodyParams.location || '').trim();
     dispatch(communityGotoActions.gotoSubreddit(location));
--- src/app/router/handlers/ToggleSubredditSubscription.js
+++ src/app/router/handlers/ToggleSubredditSubscription.js
@@ -1,7 +1,11 @@
 import { BaseHandler, METHODS } from '@r/platform/router';
 import * as subscriptionActions from 'app/actions/subscribedSubreddits';
 
+export const NAME = 'ToggleSubredditSubscription';
+
 export default class ToggleSubredditSubscription extends BaseHandler {
+  name = NAME;
+
   async [METHODS.POST](dispatch) {
     dispatch(subscriptionActions.toggleSubscription(this.bodyParams));
   }
