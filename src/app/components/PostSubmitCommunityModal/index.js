81f3d557676c2bcf687c64aa5fa7b37a9f1750c9
Phil Aquilina <philaquilina@gmail.com>
2016-06-17 18:18:14 -0700

    convert PostSubmitCommunityModal to class

--- src/app/components/PostSubmitCommunityModal/index.js
+++ src/app/components/PostSubmitCommunityModal/index.js
@@ -12,49 +12,61 @@ import './styles.less';
 const COMMUNITY_DEFAULT = 'Search for a community';
 const RECENTS_LIMIT = 7;
 
-const PostSubmitCommunityModal = (props) => {
-  const { recentSubreddits } = props;
-  return (
-    <Modal exitTo='/submit' titleText='Post to a community'>
-      <div className='PostSubmitCommunity'>
-        <div className='PostSubmitCommunity__search'>
-          <div className='icon icon-search'></div>
-          <div className='PostSubmitCommunity__search-input'>
-            <input placeholder={ COMMUNITY_DEFAULT } />
+class PostSubmitCommunityModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderSubredditRow = this.renderSubredditRow.bind(this);
+  }
+
+  render() {
+    const { recentSubreddits } = this.props;
+    return (
+      <Modal exitTo='/submit' titleText='Post to a community'>
+        <div className='PostSubmitCommunity'>
+          <div className='PostSubmitCommunity__search'>
+            <div className='icon icon-search'></div>
+            <div className='PostSubmitCommunity__search-input'>
+              <input placeholder={ COMMUNITY_DEFAULT } />
+            </div>
           </div>
+          { recentSubreddits.length ?
+              this.renderRecentSubreddits(recentSubreddits) : null }
         </div>
-        { recentSubreddits.length ? renderRecentSubreddits(recentSubreddits) : null }
-      </div>
-    </Modal>
-  );
-}
+      </Modal>
+    );
+  }
 
-const renderRecentSubreddits = (recentSubreddits) => {
-  return (
-    <div className='PostSubmitCommunity__recents'>
-      <div className='PostSubmitCommunity__recents-title'>
-        Recently visited
+  renderRecentSubreddits() {
+    return (
+      <div className='PostSubmitCommunity__recents'>
+        <div className='PostSubmitCommunity__recents-title'>
+          Recently visited
+        </div>
+        <div className='PostSubmitCommunity__recents-list'>
+          { this.props.recentSubreddits.map(this.renderSubredditRow) }
+        </div>
       </div>
-      <div className='PostSubmitCommunity__recents-list'>
-        { recentSubreddits.map(renderSubredditRow) }
-      </div>
-    </div>
-  );
-};
+    );
+  }
 
-const renderSubredditRow = ({ subredditName, iconUrl }) => {
-  const style = iconUrl ? { backgroundImage: `url(${iconUrl})` } : null;
+  renderSubredditRow({ subredditName, iconUrl }) {
+    const style = iconUrl ? { backgroundImage: `url(${iconUrl})` } : null;
 
-  return (
-    <div className='PostSubmitCommunity__recents-row'>
-      <div className='PostSubmitCommunity__recents-icon'>
-        <div className='PostSubmitCommunity__recents-icon-snoo' style={ style }></div>
+    return (
+      <div
+        className='PostSubmitCommunity__recents-row'
+        onClick={ () => this.props.onCommunityClick(subredditName) }
+        key={ subredditName }
+      >
+        <div className='PostSubmitCommunity__recents-icon'>
+          <div className='PostSubmitCommunity__recents-icon-snoo' style={ style }></div>
+        </div>
+        <div className='PostSubmitCommunity__recents-name'>
+          { `r/${subredditName}` }
+        </div>
       </div>
-      <div className='PostSubmitCommunity__recents-name'>
-        { `r/${subredditName}` }
-      </div>
-    </div>
-  );
+    )
+  }
 }
 
 
