98e04cb51257d657d7e9e896dd63a8139315f152
Phil Aquilina <philaquilina@gmail.com>
2016-07-26 12:06:43 -0700

    add some search actions back in

--- src/app/reduxMiddleware/events.js
+++ src/app/reduxMiddleware/events.js
@@ -1,25 +1,25 @@
 /* eslint callback-return: "off" */
 import * as platform from '@r/platform/actions';
 
-import * as search from 'app/actions/search';
-import * as login from 'app/actions/login';
-import * as comment from 'app/actions/comment';
 import * as pageviewEvent from 'app/actions/events/pageview';
+import * as search from 'app/actions/search';
 import * as searchEvent from 'app/actions/events/search';
+import * as login from 'app/actions/login';
 import * as loginEvent from 'app/actions/events/login';
-//import * as registerEvent from 'app/actions/events/register';
+import * as comment from 'app/actions/comment';
 import * as commentEvent from 'app/actions/events/comment';
+//import * as registerEvent from 'app/actions/events/register';
 //import * as submitEvent from 'app/actions/events/submit';
 
 const actionEventMap = {
   [platform.SET_PAGE]: pageviewEvent.pageview,
-  [search.FETCHING_SEARCH_REQUEST]: searchEvent.search,
-  [login.LOGGED_IN]: loginEvent.login,
-  //[register.REGISTERED]: registerEvent.register,
-  [comment.REPLIED]: commentEvent.reply,
   [search.OPEN_SEARCH]: searchEvent.opened,
   [search.SEARCHED]: searchEvent.executed,
   [search.CLOSE_SEARCH]: searchEvent.cancelled,
+  [search.FETCHING_SEARCH_REQUEST]: searchEvent.search,
+  [login.LOGGED_IN]: loginEvent.login,
+  [comment.REPLIED]: commentEvent.reply,
+  //[register.REGISTERED]: registerEvent.register,
   // submit post
 };
 
